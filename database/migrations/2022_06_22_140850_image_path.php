27ef40f32a234314ecf2c676b7b4c82df1a22cce
Berk Gülşen <83736291+berkgulsen@users.noreply.github.com>
2022-06-24 14:01:54 +0300

    image database foreign keys

--- database/migrations/2022_06_22_140850_image_path.php
+++ database/migrations/2022_06_22_140850_image_path.php
@@ -16,7 +16,22 @@ return new class extends Migration
         Schema::create('imagePath', function (Blueprint $table) {
             $table->id();
             $table->string('imagePath');
+            $table->unsignedBigInteger('akademi_id');
+            $table->unsignedBigInteger('sub_id');
+            $table->unsignedBigInteger('department_id');
             $table->timestamps();
+
+            $table->foreign('akademi_id')
+                  ->references('id')
+                  ->on('akademis');
+
+            $table->foreign('sub_id')
+                  ->references('id')
+                  ->on('sub');
+
+            $table->foreign('department_id')
+                  ->references('id')
+                  ->on('department');
         });
     }
 
