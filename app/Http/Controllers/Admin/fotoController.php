3bbe6c9d02fb0c2f55ac254cdf9a686982e0edf6
mustafaGordes <mustafagordes@hotmail.com>
2022-06-27 00:13:03 +0300

    resize ve 1mb limiti geldi

--- app/Http/Controllers/Admin/fotoController.php
+++ app/Http/Controllers/Admin/fotoController.php
@@ -7,6 +7,7 @@ use App\Models\ImagePath;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\File;
+use Intervention\Image\Facades\Image;
 
 class  fotoController extends Controller
 {
@@ -16,59 +17,47 @@ class  fotoController extends Controller
         $subAkademi = $request->sub;
         $bolum = $request->department;
         $tarih = $request->mezuniyetYili;
-        $i = 1;
+        $i = 1; $c = 0; $v = 0;
         if ($request->file('image')) {
             $destinationPath = 'uploads/';
             foreach ($request->file('image') as $image) {
+                $bool = 0;
+                $size = File::size($image);
+                $size = $size/1000000;
+                if($size>1){
+                    $bool = 1;
+                }
                 do {
                     $filename = str_pad($akademi, 2, '0', STR_PAD_LEFT) . str_pad($subAkademi, 2, '0', STR_PAD_LEFT) . str_pad($bolum, 3, '0', STR_PAD_LEFT) . str_pad($tarih, 4, '0', STR_PAD_LEFT) . str_pad($i, 4, '0', STR_PAD_LEFT);
                     $fileCheck = $destinationPath.$filename . '.' . 'png';
                     $i++;
                 }while(file_exists(public_path($fileCheck)));
-                $image->move($destinationPath, $filename . '.' . 'png');
-                $imagePath = new ImagePath();
-                $imagePath->imagePath = $filename;
-                $imagePath->akademi_id = $request->akademi;
-                $imagePath->sub_id = $request->sub;
-                $imagePath->department_id = $request->department;
-                $imagePath->year_id = $request->mezuniyetYili;
-                $imagePath->save();
+                if($bool==1){
+                    $c++;
+                }else{
+                    $image = Image::make($image);
+                    $image->resize(400, 400, function ($constraint) {
+                        $constraint->aspectRatio();
+                        $constraint->upsize();
+                    });
+                    $image->save($destinationPath. $filename . '.' . 'png');
+                    $imagePath = new ImagePath();
+                    $imagePath->imagePath = $filename;
+                    $imagePath->akademi_id = $request->akademi;
+                    $imagePath->sub_id = $request->sub;
+                    $imagePath->department_id = $request->department;
+                    $imagePath->year_id = $request->mezuniyetYili;
+                    $imagePath->save();
+                    $v++;
+                }
             }
         }
-        return redirect('admin-listele')->with('status',"Foto eklendi!");
+        return redirect('foto-ekle-akademik')->with('status',$v."Foto eklendi!  ". $c. "Foto eklenemedi, dosya boyutu çok büyük!");
     }
 
     public function edit(){
         return view('admin.foto.add');
     }
-
-//    public function list(Request $request){
-//        $akademi = $request->akademi;
-//        $subAkademi = $request->sub;
-//        $bolum = $request->department;
-//        $tarih = $request->mezuniyetYili;
-//        $i = 1;
-//        if ($request->file('image')) {
-//            $destinationPath = 'uploads/';
-//            foreach ($request->file('image') as $image) {
-//                do {
-//                    $filename = str_pad($akademi, 2, '0', STR_PAD_LEFT) . str_pad($subAkademi, 2, '0', STR_PAD_LEFT) . str_pad($bolum, 3, '0', STR_PAD_LEFT) . str_pad($tarih, 4, '0', STR_PAD_LEFT) . str_pad($i, 4, '0', STR_PAD_LEFT);
-//                    $fileCheck = $destinationPath.$filename . '.' . 'png';
-//                    $i++;
-//                }while(file_exists(public_path($fileCheck)));
-//                $image->move($destinationPath, $filename . '.' . 'png');
-//                $imagePath = new ImagePath();
-//                $imagePath->imagePath = $filename;
-//                $imagePath->akademi_id = $request->akademi;
-//                $imagePath->sub_id = $request->sub;
-//                $imagePath->department_id = $request->department;
-//                $imagePath->year_id = $request->mezuniyetYili;
-//                $imagePath->save();
-//            }
-//        }
-//
-//        return view('admin.super.list',compact('user'));
-//    }
 
     public function delete($id){
         $foto = foto::find($id);
