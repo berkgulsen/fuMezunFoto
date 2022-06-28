461fb491bbb016e0ec1bf2f1757d4c2b3956bca8
Berk Gülşen <83736291+berkgulsen@users.noreply.github.com>
2022-06-28 13:48:00 +0300

    admin panel single photo update modal

--- resources/views/admin/foto/show.blade.php
+++ resources/views/admin/foto/show.blade.php
@@ -35,24 +35,48 @@
     </tr>
     </thead>
     <tbody>
+    <?php $count = 0 ?>
     @foreach($data['fotos'] as $foto)
     <tr>
         <th scope="row"></th>
         <td>
-            <img src="{{asset('assets/uploads/'.$foto->imagePath.'.png')}}"  width="150px" alt="resim">
+            <img src="{{asset('/uploads/'.$foto->imagePath.'.png')}}"  width="150px" alt="resim">
         </td>
         <td>Otto</td>
         <td>
             <a href="{{route('foto-single',[$foto->id])}}" title="Görüntüle" class="btn btn-sm btn-success"><i class="fa fa-eye"></i></a>
-            <button onclick="myFunction()" title="Düzenle" class="btn btn-sm btn-primary"><i class="fa fa-edit"></i></button>
-            <a href="{{url('foto-delete/'.$foto->id)}}" title="Sil" class="btn btn-sm btn-danger"><i class="fa fa-times"></i></a>
-            <form id="desc" action="{{url('foto-edit/'.$foto->id)}}" method="post" enctype="multipart/form-data">
-                @csrf
-                <div class="col-md-12">
-                    <input type="file" class="form_control" name="image">
+            <button type="button" title="Düzenle" class="btn btn-sm btn-primary" data-toggle="modal" data-target="#myModal{{$count}}">
+                <i class="fa fa-edit"></i>
+            </button>
+            <!-- Modal -->
+            <div class="modal fade" id="myModal{{$count}}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            {{$foto->id}}
+                            <form id="desc" action="{{url('foto-edit/'.$foto->id)}}" method="post" enctype="multipart/form-data">
+                                @csrf
+                                <div class="col-md-12">
+                                    <input type="file" class="form_control" name="image">
+                                </div>
+                                <input type="submit" value="Upload">
+                            </form>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                            <button type="button" class="btn btn-primary">Save changes</button>
+                        </div>
+                    </div>
                 </div>
-                <input type="submit" value="Upload">
-            </form>
+            </div>
+            <?php $count++; ?>
+            <a href="{{url('foto-delete/'.$foto->id)}}" title="Sil" class="btn btn-sm btn-danger"><i class="fa fa-times"></i></a>
         </td>
     </tr>
     @endforeach
